f018cf0abdf4754a978908b1576d320d904a5456
navaneeth001 <navaneethpqln@gmail.com>
2024-02-27 17:20:37 +0530

    more findings

---
+++ Cti-taxiimitre.py
@@ -0,0 +1,11 @@
+from taxii2client.v20 import Server
+from taxii2client.v20 import Collection
+
+server = Server("https://cti-taxii.mitre.org/stix")
+collection = Collection("https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e")
+print('collection media_types',collection.media_types)
+print('collection is',collection)
+print('api roots are',server.api_roots)
+print('Server contact',server.contact)
+print(server.title)
+print(server.description)
--- taxii_client.py
+++ taxii_client.py
@@ -2,20 +2,19 @@ from cabby import create_client
 api_key='bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723'
 def main():
     testClient = create_client(
-    discovery_url='https://pulsedive.com/taxii2'+'/?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1&pretty=1&key=' + api_key,
-    # use_https=True,
-    # discovery_path='/',
-    headers={'Authorization': f'Apikey {api_key}'}
+    discovery_url='https://pulsedive.com/taxii2?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1',
+    use_https=True,
+    version='2.1',
+    discovery_path='/',
+    headers={'Authorization':'Basic YmJjZmY3NGNmODQ0MmVkY2M4ZDUyYTRiNjFlYzlhNTg5MTJlMGIwMThiYmI0NzNjMGYwODEzNjU5NTY3NjcyMw==','Accept':'application%2Ftaxii%2Bjson%3Bversion%3D2.1'}
     )
-    collections =testClient.set_auth(password='bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723' )
-
-
-    # discover_services = testClient.discover_services()
+    
+    discover_services = testClient.discover_services()
     collections =testClient.get_collections()
     services =testClient.get_services()
     for collection in collections:
             print(collection)
-    # print('discover_services',discover_services)        
+    print('discover_services',discover_services)        
     print('services',services)
 if __name__ == "__main__":
     main()
