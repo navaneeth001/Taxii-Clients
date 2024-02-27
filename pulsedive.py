c503ffbfa7778915951e98e4b3e611c9a7945ef8
navaneeth001 <navaneethpqln@gmail.com>
2024-02-27 23:35:27 +0530

    sync

--- pulsedive.py
+++ pulsedive.py
@@ -1,6 +1,14 @@
 from taxii2client.v21 import Server
 from taxii2client.v20 import Collection
+import http.client
 
+conn = http.client.HTTPConnection('https://pulsedive.com/taxii2/api/collections?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1&pretty=1&key=bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723'
+                                  )
+
+headers = {
+    "Accept": "application%2Ftaxii%2Bjson%3Bversion%3D2.1",
+    "Authorization": "Basic YmJjZmY3NGNmODQ0MmVkY2M4ZDUyYTRiNjFlYzlhNTg5MTJlMGIwMThiYmI0NzNjMGYwODEzNjU5NTY3NjcyMw==",
+}
 server = Server(url='https://pulsedive.com/taxii2/api/collections?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1&pretty=1&key=bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723')
 collection = Collection("https://pulsedive.com/taxii2/api/collections?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1&pretty=1&key=bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723")
 
