a4a651fe7eca84b91e04be5c04e5a9f1449c2aae
Prakash <Navaneeth.Prakash@Honeywell.com>
2024-03-01 11:18:54 +0530

    added output file

--- cti-taxii-client.py
+++ cti-taxii-client.py
@@ -1,15 +1,11 @@
 from taxii2client.v21 import Server
-from taxii2client.v21 import Collection
+import json
 
 server = Server("https://otx.alienvault.com/taxii")
 print(server.title)
 print(server.description)
-print(server.api_roots[0])
-print(server.contact)
-# print('collection1 objects are',collection1.get_objects())
 api_root = server.api_roots[0]
 collection = api_root.collections[0]
-print('collection name',collection)
-# print('data objects from collection are',collection.get_objects())
-print(collection.can_read,collection.can_write,collection.media_types)
+temp=json.dumps(collection.get_objects())
+print(temp)
 
--- pulsedive.py
+++ pulsedive.py
@@ -1,8 +1,7 @@
 from taxii2client.v21 import Server
-from taxii2client.v20 import Collection
+from taxii2client.v21 import Collection
 
-server = Server(url='https://pulsedive.com/taxii2/api/collections?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1&pretty=1&key=bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723')
 collection = Collection("https://pulsedive.com/taxii2/api/collections?accept=application%2Ftaxii%2Bjson%3Bversion%3D2.1&pretty=1&key=bbcff74cf8442edcc8d52a4b61ec9a58912e0b018bbb473c0f08136595676723")
 
 print(collection.get_objects())
-# print(server.description)
+
